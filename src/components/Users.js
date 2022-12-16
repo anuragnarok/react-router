8ec2f5889661997cb1e22fbb172c15c3ed54105d
Anurag <anuragchakravorty000@gmail.com>
2022-12-16 06:48:08 +0530

    .

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet , useSearchParams} from 'react-router-dom'
 
 const Users = () => {
+    const [searchParams, setSearchParams] =useSearchParams()
+    const ShowActiveUsers = searchParams.get('filter') === 'active'
   return (
     <div>
         <h1>User 1</h1>
         <h1>User 2</h1>
         <h1>User 3</h1>
         <Outlet/>
+        <div>
+            <button onClick={()=> setSearchParams({filter: 'active'})}>Active users</button>
+            <button onClick={()=> setSearchParams({})}>Reset filter</button>
+        </div>
+        {
+            ShowActiveUsers ? <h2>Showing Active users</h2> : <h2>Showing all users</h2>
+        }
     </div>
   )
 }
